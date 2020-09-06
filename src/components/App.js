0b1d4b7fb3f9797da983bdca3fab5542945e127d
Hüzeyfe TAŞ <huzeyfetas@outlook.com>
2020-09-06 19:16:07 +0300

    Axios added and http request done

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,34 @@ import React, { Component, Fragment } from "react";
 import Navbar from "./Navbar";
 import Users from "./Users";
 import User from "./User";
+import axios from "axios";
 
 export class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			users: [],
+		};
+	}
+	componentDidMount() {
+		axios
+			.get("https://api.github.com/users")
+			.then((res) => {
+				// console.log(res);
+				// console.log(res.data);
+				this.setState({
+					users: res.data,
+				});
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
 	render() {
 		return (
 			<>
 				<Navbar />
-				<Users />
+				<Users users={this.state.users} />
 			</>
 		);
 	}
