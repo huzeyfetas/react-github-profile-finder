d06954f6d1068badc14ea96286a79c60e2423b9f
Hüzeyfe TAŞ <huzeyfetas@outlook.com>
2020-09-13 19:20:28 +0300

    Search compt. updated with react hooks

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,68 +1,55 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-export class Search extends Component {
-	constructor(props) {
-		super(props);
-		this.inputOnChange = this.inputOnChange.bind(this);
-		this.formOnSubmit = this.formOnSubmit.bind(this);
-		this.state = {
-			keyword: "",
-		};
-	}
+const Search = ({ setAlert, searchUser, isShow, clearResults }) => {
+	const [keyword, setKeyword] = useState("");
 
-	inputOnChange(event) {
-		this.setState({
-			keyword: event.target.value,
-		});
-	}
+	const inputOnChange = (event) => {
+		setKeyword(event.target.value);
+	};
 
-	formOnSubmit(event) {
+	const formOnSubmit = (event) => {
 		event.preventDefault();
-		let word = this.state.keyword;
+		let word = keyword;
 		word = word.trim();
 		if (word === "") {
-			this.props.setAlert(
+			setAlert(
 				"Search box is empty.Are you sure you are searching right? If you are seeing this error message right now, either you or we are doing something wrong. If you are sure ,be cool, I think we did something wrong.",
 				"warning",
 			);
 			return;
 		}
 
-		this.props.searchUser(this.state.keyword);
-		this.setState({
-			keyword: "",
-		});
-	}
+		searchUser(keyword);
+		setKeyword("");
+	};
 
-	render() {
-		return (
-			<div className="container mt-2">
-				<form onSubmit={this.formOnSubmit}>
-					<div className="input-group">
-						<input
-							type="text"
-							value={this.state.keyword}
-							onChange={this.inputOnChange}
-							className="form-control"
-						/>
-						<div className="input-group-append">
-							<button type="submit" className="btn btn-primary">
-								Search
-							</button>
-						</div>
+	return (
+		<div className="container mt-2">
+			<form onSubmit={formOnSubmit}>
+				<div className="input-group">
+					<input
+						type="text"
+						value={keyword}
+						onChange={inputOnChange}
+						className="form-control"
+					/>
+					<div className="input-group-append">
+						<button type="submit" className="btn btn-primary">
+							Search
+						</button>
 					</div>
-				</form>
-				{this.props.isShow && (
-					<button
-						onClick={this.props.clearResults}
-						className="btn btn-md btn-danger btn-block mt-2"
-					>
-						Clear Results
-					</button>
-				)}
-			</div>
-		);
-	}
-}
+				</div>
+			</form>
+			{isShow && (
+				<button
+					onClick={clearResults}
+					className="btn btn-md btn-danger btn-block mt-2"
+				>
+					Clear Results
+				</button>
+			)}
+		</div>
+	);
+};
 
 export default Search;
